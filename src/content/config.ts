ff7e284cbd6ae3407c2c020318c60dd437c1849f
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-06 22:39:57 +0700

    fix: add "/" after "${import.meta.env.BASE_URL}"

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,7 @@ const posts = defineCollection({
         title: z.string(),
         description: z.string(),
         date: z.coerce.date(),
-        image: z.preprocess((i) => `${import.meta.env.BASE_URL}${i}`, z.string()),
+        image: z.preprocess((i) => `${import.meta.env.BASE_URL}/${i}`, z.string()),
         color: z.string().optional(),
         pinned: z.any().optional(),
     }),
