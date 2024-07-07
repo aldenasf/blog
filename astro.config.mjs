081067aaeecc3fcce566e466be6a30ecc63e3cf8
Alden <63831735+AldenizenMC@users.noreply.github.com>
2024-07-07 09:47:21 +0700

    feat: add @astrojs/sitemap for sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://aldenasf.github.io/",
     base: "/blog",
-    integrations: [tailwind(), mdx(), icon()],
+    integrations: [tailwind(), mdx(), icon(), sitemap()],
 });
